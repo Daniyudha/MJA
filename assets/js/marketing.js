0f3c469d7361258a94019b349129dcf83b8a68f3
Daniyudha <dani.yudha33@gmail.com>
2021-05-19 16:41:20 +0700

    ar

--- assets/js/marketing.js
+++ assets/js/marketing.js
@@ -38,31 +38,59 @@ $('.owl-carousel').owlCarousel({
 	$('.owl-item').eq(idx - 2).addClass('smallLeft');
   }
   
-  $('.slider').slick({
+  $('.slider-single').slick({
 	slidesToShow: 1,
 	slidesToScroll: 1,
 	arrows: true,
 	fade: false,
-	asNavFor: '.slider-nav-thumbnails',
+	adaptiveHeight: true,
+	infinite: false,
+   useTransform: true,
+	speed: 400,
+	cssEase: 'cubic-bezier(0.77, 0, 0.18, 1)',
 });
 
-$('.slider-nav-thumbnails').slick({
-	slidesToShow: 5,
-	slidesToScroll: 1,
-	asNavFor: '.slider',
-	dots: true,
-	focusOnSelect: true
+$('.slider-nav')
+	.on('init', function(event, slick) {
+		$('.slider-nav .slick-slide.slick-current').addClass('is-active');
+	})
+	.slick({
+		slidesToShow: 7,
+		slidesToScroll: 7,
+		dots: false,
+		focusOnSelect: false,
+		infinite: false,
+		responsive: [{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 5,
+				slidesToScroll: 5,
+			}
+		}, {
+			breakpoint: 640,
+			settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4,
+		   }
+		}, {
+			breakpoint: 420,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+	   }
+		}]
+	});
+
+$('.slider-single').on('afterChange', function(event, slick, currentSlide) {
+	$('.slider-nav').slick('slickGoTo', currentSlide);
+	var currrentNavSlideElem = '.slider-nav .slick-slide[data-slick-index="' + currentSlide + '"]';
+	$('.slider-nav .slick-slide.is-active').removeClass('is-active');
+	$(currrentNavSlideElem).addClass('is-active');
 });
 
-// Remove active class from all thumbnail slides
-$('.slider-nav-thumbnails .slick-slide').removeClass('slick-active');
+$('.slider-nav').on('click', '.slick-slide', function(event) {
+	event.preventDefault();
+	var goToSingleSlide = $(this).data('slick-index');
 
-// Set active class to first thumbnail slides
-$('.slider-nav-thumbnails .slick-slide').eq(0).addClass('slick-active');
-
-// On before slide change match active thumbnail to current slide
-$('.slider').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-	var mySlideNumber = nextSlide;
-	$('.slider-nav-thumbnails .slick-slide').removeClass('slick-active');
-	$('.slider-nav-thumbnails .slick-slide').eq(mySlideNumber).addClass('slick-active');
+	$('.slider-single').slick('slickGoTo', goToSingleSlide);
 });
